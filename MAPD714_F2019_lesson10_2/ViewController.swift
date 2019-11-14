6bd88a2b6c4a8a0778b7ef1a8f361d8865637204
Nisha Tandel <angeltandel64.nt@gmail.com>
2019-11-13 20:21:22 -0500

    Initial Commit

---
+++ MAPD714_F2019_lesson10_2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MAPD714_F2019_lesson10_2
+//
+//  Created by Nisha Tandel on 2019-11-13.
+//  Copyright © 2019 Nisha Tandel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
